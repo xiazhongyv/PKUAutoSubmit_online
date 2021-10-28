36d07f6ff0b9be46a5b6136b958a06e1b3a057cd
xiazhongyv <nife@pku.edu.cn>
2021-10-28 11:18:17 +0800

    autosubmit

---
+++ main.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8
+import env_check
+from configparser import ConfigParser
+from selenium.webdriver.chrome.options import Options
+from func import *
+import warnings
+import sys
+import os
+import re
+warnings.filterwarnings('ignore')
+
+
+def sys_path(browser):
+    path = f'./{browser}/bin/'
+    if sys.platform.startswith('win'):
+        return path + f'{browser}.exe'
+    elif sys.platform.startswith('linux'):
+        return path + f'{browser}'
+    elif sys.platform.startswith('darwin'):
+        return path + f'{browser}'
+    else:
+        raise Exception('暂不支持该系统')
+
+
+def go(config):
+    conf = ConfigParser()
+    conf.read(config, encoding='utf8')
+
+    userName, password = dict(conf['login']).values()
+    campus, reason = dict(conf['common']).values()
+    destination, track = dict(conf['out']).values()
+    habitation, district, street = dict(conf['in']).values()
+    capture = conf.getboolean('capture', '是否需要备案历史截图')
+    path = conf['capture']['截图保存路径']
+    wechat = conf.getboolean('wechat', '是否需要微信通知')
+    sckey = conf['wechat']['SCKEY']
+
+    run(driver_pjs, userName, password, campus, reason, destination, track,
+        habitation, district, street, capture, path, wechat, sckey)
+
+
+if __name__ == '__main__':
+
+    # driver_pjs = webdriver.PhantomJS(
+    #     executable_path=sys_path(browser="phantomjs"),
+    #     service_args=['--ignore-ssl-errors=true', '--ssl-protocol=TLSv1'])
+
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver_pjs = webdriver.Edge(
+            options=chrome_options,
+            executable_path=sys_path(browser="chromedriver"),
+            service_args=['--ignore-ssl-errors=true', '--ssl-protocol=TLSv1'])
+    print('Driver Launched\n')
+
+    lst_conf = sorted([
+        fileName for fileName in os.listdir()
+        if re.match(r'^config[0-9][0-9]*\.ini$', fileName)
+    ],
+        key=lambda x: int(re.findall(r'[0-9]+', x)[0]))
+
+    print(f'读取到{len(lst_conf)+1}份配置文件\n')
+    print('||第1个学生备案||')
+    go('config.ini')
+
+    if lst_conf:
+        for num, config in enumerate(lst_conf):
+            print(f'||第{num+2}个学生备案||')
+            go(config)
+
+    driver_pjs.quit()
