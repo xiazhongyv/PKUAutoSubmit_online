9ac665ebfae229258b0033c9bcd26e930bf887c9
xiazhongyv <45617755+xiazhongyv@users.noreply.github.com>
2021-10-28 13:44:31 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-sudo cp -p ./chromedriver/bin/chromedriver /usr/bin/
-chmod -R 777 /usr/bin/chromedriver
-chmod -R 777 ./chromedriver/bin/chromedriver
-python main.py --ID ${{ID}} --PASSWORD ${{PASSWORD}} --SENDKEY ${{SENDKEY}}
